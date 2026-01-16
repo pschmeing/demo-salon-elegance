95d3d8b0c57ec111b888575f7706b7f1217b01bd
pschmeing <83138772+pschmeing@users.noreply.github.com>
2026-01-16 15:38:38 +0100

    x

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/deutsch-helper/",
   server: {
     host: "::",
     port: 8080,
